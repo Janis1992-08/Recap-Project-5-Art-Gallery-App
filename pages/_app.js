652d7503482910364a035abce307515bac128ea8
Marta Jäkel <martajaekel@gmail.com>
2023-11-28 12:09:50 +0100

    set artPieceInfo state and make it global

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import useLocalStorageState from "use-local-storage-state";
 import GlobalStyle from "../styles";
 
 export default function App({ Component, pageProps }) {
+  const [artPieceInfo, setArtPieceInfo] = useLocalStorageState("art-piece-info", {
+    defaultValue: [] }
+  );
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} artPieceInfo={artPieceInfo} />
     </>
   );
 }
