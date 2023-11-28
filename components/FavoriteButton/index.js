cae9bdf5385618459058cc93071b240c010ecb5e
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-28 16:38:06 +0100

    Fixed Heart Icon

--- components/FavoriteButton/index.js
+++ components/FavoriteButton/index.js
@@ -1,15 +1,10 @@
-import YourSvgImage from "../../assets/heart.svg"
 import Image from "next/image.js";
+import heartUrl from "../../assets/heart.svg?url";
+
 export default function FavoriteButton({ isFavorite, onHandleFavorite }) {
   return (
     <button type="button" onClick={onHandleFavorite} isFavorite={isFavorite}>
-      <Image
-        src={YourSvgImage}
-        alt="Heart Icon"
-        width={30}
-        height={30}
-      />
-      
+      <Image src={heartUrl} alt="Heart Icon" width={30} height={30} />
     </button>
   );
 }
