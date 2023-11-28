81647b07bab997c65d94358cb997266d1f1928ac
Jan Maaß <git@antipost.de>
2023-11-28 12:35:07 +0100

    Update favorite listing page

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,14 +1,18 @@
+import React from "react";
 import ArtPieces from "@/components/ArtPieces";
+// import 'global state'
 
-export default function FavoritesPage({ pieces }) {
+export default function FavoritesPage() {
+  // const {artPiecesInfo} = globalstate
+  const favoriteArtPieces = artPiecesInfo.filter(piece => piece.isFavorite);
   return (
     <>
       <h1>Favorites</h1>
       <ul>
         <li>
-          <ArtPieces pieces={pieces}></ArtPieces>
+          <ArtPieces pieces={favoriteArtPieces} />
         </li>
       </ul>
     </>
-  )
-}
+  );
+};
